3d8c305b3d9f4179c3662cefb0415e4d9900c0e0
Ben Skerritt <ben.skerritt77@gmail.com>
2017-05-17 12:09:11 +0100

    Set up ESLint to make it run automatically on npm start

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import numeral from 'numeral';
 
+/* eslint-disable no-console */
+
 const drugMoney = numeral(1000).format('$0,0.00');
-debugger;
 console.log(`I would pay ${drugMoney} for a ton of charlie!`);
